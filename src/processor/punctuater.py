ede9f0d8a2a605e7884d829fed9a406228d4220c
nogafeiner <noga.feiner@signals-analytics.com>
2022-06-01 22:21:24 +0300

    use post request to punctuate text

--- src/processor/punctuater.py
+++ src/processor/punctuater.py
@@ -1,19 +1,27 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 
-url = 'http://bark.phon.ioc.ee/punctuator'
 
-headers = CaseInsensitiveDict()
-headers["Content-Type"] = "application/json"
+def punctuate_text(text):
+    PUNCTUATOR_URL = 'http://bark.phon.ioc.ee/punctuator'
 
-data = "text=hello world what is the status have a good day and nice cookie what a wonderful world"
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/json"
+    PARAMS = {'text': text}
 
-resp = requests.post(url, headers=headers, data=data)
+    # sending get request and saving the response as response object
+    resp = requests.post(url=PUNCTUATOR_URL, headers=headers, params=PARAMS)
 
-print(resp.status_code)
+    response_punctuated_text = str(resp.content)
+    response_punctuated_text = response_punctuated_text.replace("b'", "")
 
+    print(resp.status_code)
+    return response_punctuated_text
 
 #curl -d "text=hello%20world" http://bark.phon.ioc.ee/punctuator
 
 
-
+if __name__ == "__main__":
+    text = "hello world what is the status have a good day and nice cookie what a wonderful world"
+    punctuated_text_result = punctuate_text(text)
+    print(punctuated_text_result)
