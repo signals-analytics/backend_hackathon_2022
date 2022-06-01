66b661cf70fc7f129dad8ab07a424983b0ef5364
Idan Weinstein <idan.weinstein@skai.io>
2022-06-01 14:59:41 +0300

    fetch transcripts and save to json

--- src/fetcher/start_video_processing.py
+++ src/fetcher/start_video_processing.py
@@ -1,22 +1,29 @@
 import argparse
 import logging
 
+from src.fetcher.transcript_video_fetcher import fetch_transcript_by_id, write_transcripts_to_json
+
 logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 
-if __name__ == "__main__":
-    import sys
-    print(sys.argv)
+
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--video_url", required=True)
-    args = parser.parse_args()
-
-    video_url = args.video_url
-    video_id =  video_url.split("=")[1]
-
-    logger.info('fetching transcript from video_url:' + video_url)
-    logger.info('video_id:' + video_id)
+    parser.add_argument("--output_path", required=True)
+    return parser.parse_args()
 
 
+def parse_video_id(args):
+    return args.video_url.split("=")[1]
 
+
+if __name__ == "__main__":
+    args = parse_args()
+    logger.info('fetching transcript from video_url:' + args.video_url)
+    video_id = parse_video_id(args)
+    logger.info('Going to fetch transcripts for video_id:' + video_id)
+    transcripts = fetch_transcript_by_id(video_id)
+    logger.info('Going to write transcripts into json')
+    write_transcripts_to_json(transcripts, args.output_path)
     logger.info('done')
