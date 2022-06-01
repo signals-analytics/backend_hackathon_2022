94742fb81995479f5d807b153a1a1f2097c320ea
sergeily-io <sergei.lyubofeev@skai.io>
2022-06-01 19:56:38 +0300

    chrome extension

--- extension_chrome_YouSentiment/src/index.js
+++ extension_chrome_YouSentiment/src/index.js
@@ -4,60 +4,59 @@ const errors = document.querySelector(".errors");
 const loading = document.querySelector(".loading");
 const results = document.querySelector(".result-container");
 const res = document.querySelector(".res");
-//
-const link = document.querySelector(".link");
-const h = document.querySelector(".headers");
 
 results.style.display = "none";
 loading.style.display = "none";
 errors.textContent = "";
 
-//rundeck job url
-const jobURL = 'http://rundeck.signalsanalytics.co:4440/api/17/job/5249594e-5e9e-4698-917a-6ae9ee8d5e76/executions';
-
-//headers
-const post_headers = {
-    "Content-Type": "application/json",
-    "X-Rundeck-Auth-Token": "HJyzq9aBHPhxr2v9wXTGqBIzrKqIM12O",
-    "Accept": "application/json"
-}
-
-const data = JSON.stringify({
-    "argString": "-libpath /opt/signals/hadoop-processing/lib/ -video_url https://www.youtube.com/watch?v=wTXmkn6B7dI&lc=UgzyiS_48Q1Q6bdAMwt4AaABAg&ab_channel=Hyram -output_path /tmp/"
-});
-
 // grab the form
 const form = document.querySelector(".form-data");
 
 // grab the video URL
-const videoUrl = document.querySelector(".video-url");
+let videoUrl = document.querySelector(".video-url");
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    videoUrl.value = tabs[0].url;
+    // use `url` here inside the callback because it's asynchronous!
+});
 
-const config = {
-    method: 'POST',
-    url: jobURL,
-    headers: post_headers,
-    data: data
-};
 
 // declare a method to search
-const searchForSentiment = async (config) => {
+const searchForSentiment = async (videoUrl) => {
     loading.style.display = "block";
     errors.textContent = "";
+
+    //rundeck job url
+    const jobURL = 'http://rundeck.signalsanalytics.co:4440/api/17/job/5249594e-5e9e-4698-917a-6ae9ee8d5e76/executions';
+
+    //headers
+    const post_headers = {
+        "Content-Type": "application/json",
+        "X-Rundeck-Auth-Token": "HJyzq9aBHPhxr2v9wXTGqBIzrKqIM12O",
+        "Accept": "application/json"
+    }
+
+    const argString = `-libpath /opt/signals/hadoop-processing/lib/ -video_url ${videoUrl} -output_path /tmp/`;
+    const data = JSON.stringify({
+        "argString": argString
+    });
+
+    const config = {
+        method: 'POST',
+        url: jobURL,
+        headers: post_headers,
+        data: data
+    };
 
     axios(config)
         .then((response) => {
             loading.style.display = "none";
-            res.textContent = response.data.value;
+            res.textContent = response.data.permalink;
             results.style.display = "block";
         })
         .catch((error) => {
             loading.style.display = "none";
             results.style.display = "none";
             errors.textContent = "error " + error;
-
-            //TODO: (ls) -> remove
-            link.textContent = jobURL;
-            h.textContent = post_headers.Accept + "-" + post_headers["Content-Type"] + "-" + post_headers["X-Rundeck-Auth-Token"];
 
             console.log(error.stack)
         })
@@ -66,8 +65,15 @@ const searchForSentiment = async (config) => {
 // declare a function to handle form submission
 const handleSubmit = async e => {
     e.preventDefault();
-    await searchForSentiment(config);
+    await searchForSentiment(videoUrl.value);
     console.log(videoUrl.value);
 };
+
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
 
 form.addEventListener("submit", e => handleSubmit(e));
