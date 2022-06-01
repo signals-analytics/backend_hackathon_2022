338d3b000df7133e7d04f74e800bccf4650c36be
sergeily-io <sergei.lyubofeev@skai.io>
2022-06-01 17:43:04 +0300

    chrome extension

--- extension_chrome_YouSentiment/src/index.js
+++ extension_chrome_YouSentiment/src/index.js
@@ -1,16 +1,19 @@
 import axios from "axios";
 
-// const api = "http://rundeck.signalsanalytics.co:4440/api/17/job/"+rundeck_job_uuid+"/executions";
-const rundeckApi = "http://rundeck.signalsanalytics.co:4440/project/platform/job/show/5249594e-5e9e-4698-917a-6ae9ee8d5e76";
-
 const errors = document.querySelector(".errors");
 const loading = document.querySelector(".loading");
 const results = document.querySelector(".result-container");
 const res = document.querySelector(".res");
+//
+const link = document.querySelector(".link");
+const h = document.querySelector(".headers");
 
 results.style.display = "none";
 loading.style.display = "none";
 errors.textContent = "";
+
+//rundeck job url
+const jobURL = 'http://rundeck.signalsanalytics.co:4440/api/17/job/5249594e-5e9e-4698-917a-6ae9ee8d5e76/executions';
 
 //headers
 const post_headers = {
@@ -19,9 +22,9 @@ const post_headers = {
     "Accept": "application/json"
 }
 
-const data = {
-    "argString" : "-libpath /opt/signals/hadoop-processing/lib/ -video_url https://www.youtube.com/watch?v=wTXmkn6B7dI&lc=UgzyiS_48Q1Q6bdAMwt4AaABAg&ab_channel=Hyram -output_path /tmp/"
-}
+const data = JSON.stringify({
+    "argString": "-libpath /opt/signals/hadoop-processing/lib/ -video_url https://www.youtube.com/watch?v=wTXmkn6B7dI&lc=UgzyiS_48Q1Q6bdAMwt4AaABAg&ab_channel=Hyram -output_path /tmp/"
+});
 
 // grab the form
 const form = document.querySelector(".form-data");
@@ -29,19 +32,20 @@ const form = document.querySelector(".form-data");
 // grab the video URL
 const videoUrl = document.querySelector(".video-url");
 
+const config = {
+    method: 'POST',
+    url: jobURL,
+    headers: post_headers,
+    data: data
+};
 
-// declare a method to search by country name
-const searchForSentiment = async videoUrl => {
+
+// declare a method to search
+const searchForSentiment = async (config) => {
     loading.style.display = "block";
     errors.textContent = "";
 
-    axios.post(
-        rundeckApi,
-        data,
-        {
-            headers: post_headers
-        }
-    )
+    axios(config)
         .then((response) => {
             loading.style.display = "none";
             res.textContent = response.data.value;
@@ -52,38 +56,18 @@ const searchForSentiment = async videoUrl => {
             results.style.display = "none";
             errors.textContent = "error " + error;
 
+            //TODO: (ls) -> remove
+            link.textContent = jobURL;
+            h.textContent = post_headers.Accept + "-" + post_headers["Content-Type"] + "-" + post_headers["X-Rundeck-Auth-Token"];
+
             console.log(error.stack)
         })
-
-
-/*    try {
-        console.log("ddddddd")
-        const response = await axios.post(
-            rundeckApi,
-            {
-                argString: argString
-            },
-            {
-                headers: post_headers
-            }
-        );
-
-        loading.style.display = "none";
-        res.textContent = response.data.value;
-        results.style.display = "block";
-
-    } catch (error) {
-        loading.style.display = "none";
-        results.style.display = "none";
-        errors.textContent = "error " + error;
-
-    }*/
 };
 
 // declare a function to handle form submission
 const handleSubmit = async e => {
     e.preventDefault();
-    searchForSentiment(videoUrl.value);
+    await searchForSentiment(config);
     console.log(videoUrl.value);
 };
 
